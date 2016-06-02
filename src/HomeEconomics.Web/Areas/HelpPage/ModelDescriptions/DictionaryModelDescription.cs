ce1148424b8635f04f1a65bf9c40509e3c71f930
Ken LeFebvre <kenneth@lefebvre.us>
2016-06-02 08:07:18 -0400

    Replace AspNetCore solution with new ASP.NET 4.6 MVC solution

---
+++ src/HomeEconomics.Web/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HomeEconomics.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ src/HomeEconomics.Web/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(HomeEconomics.Web.Startup))]
+
+namespace HomeEconomics.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
